4ad5e1f846150d47495be420cfd896b5105fb772
Alfie <alfiecollins44@gmail.com>
2024-10-28 11:15:05 +0000

    text component

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -2,7 +2,14 @@ import { View, Text } from 'react-native';
 
 export default function App(){
   return <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <View style={{ width:200, height:200, backgroundColor: "lightblue" }}></View>
-      <View style={{ width:200, height:200, backgroundColor: "lightgreen" }}></View>
+      <View style={{ width:200, height:200, backgroundColor: "lightblue", justifyContent:"center", alignItems:"center" }}>
+        <Text>Text One</Text>
+      </View>
+      <View style={{ width:200, height:200, backgroundColor: "lightgreen", justifyContent:"center", alignItems:"center" }}>
+        <Text>
+          <Text style={{ color:"red" }}>Text Two </Text>
+           is Nested
+        </Text>
+      </View>
   </View>;
 }
