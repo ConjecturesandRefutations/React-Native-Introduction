7a34d9387d35dd2fd1ace0799a0a96476e0fefd7
Alfie <alfiecollins44@gmail.com>
2024-10-29 12:55:33 +0000

    image components

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,4 +1,5 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image, ImageBackground } from 'react-native';
+const logoImg = require("./assets/adaptive-icon.png")
 
 export default function App(){
   return <View style={{ flex: 1, backgroundColor: "plum" }}>
@@ -11,5 +12,11 @@ export default function App(){
            is Nested
         </Text>
       </View>
+
+      {/* <Image source={logoImg} style="width: 300, height: 300"/>
+      <Image source={{ uri: "https://picsum.photos/300" }} style="width: 300, height: 300"/> */}
+      <ImageBackground source={logoImg} style={{ flex: 1 }}>
+          <Text>IMAGE TEXT</Text>
+      </ImageBackground>
   </View>;
 }
