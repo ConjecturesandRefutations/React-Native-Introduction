469224be918c42b3c273339c97272dc1781c8063
Alfie <alfiecollins44@gmail.com>
2025-01-27 22:16:26 +0000

    stylesheet API

---
+++ Stylesheet/App.js
@@ -0,0 +1,13 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function App(){
+  return(
+    <View style={styles.container}>
+      <Text>Stylesheet API</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{ flex:1, backgroundColor: "plum", padding: 60 }
+})
