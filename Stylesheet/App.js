3e2e4a98458e0a9a096c7d45773e81132a93794a
Alfie <alfiecollins44@gmail.com>
2025-02-10 10:36:07 +0000

    multiple styles

--- Stylesheet/App.js
+++ Stylesheet/App.js
@@ -3,11 +3,32 @@ import { Text, View, StyleSheet } from "react-native";
 export default function App(){
   return(
     <View style={styles.container}>
-      <Text>Stylesheet API</Text>
+      <View style={[styles.box, styles.lightBlueBg]}>
+        <Text>Light Blue Box</Text>
+      </View>
+      <View style={[styles.box, styles.lightGreenBg]}>
+        <Text>Light Green Box</Text>
+      </View>
+      <View style={[styles.box, styles.lightRedBg]}>
+        <Text>Learning React Native</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container:{ flex:1, backgroundColor: "plum", padding: 60 }
-})
+  container:{ flex:1, backgroundColor: "plum", padding: 60 },
+  box:{
+    width:100,
+    height:100,
+    padding:10
+  },
+  lightBlueBg:{ backgroundColor: "lightblue"},
+  lightGreenBg:{ 
+    backgroundColor: "lightgreen"
+},
+  lightRedBg:{ 
+    backgroundColor: "salmon"
+    }
+}
+)
