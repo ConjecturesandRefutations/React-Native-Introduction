75a0343e76cd0a93e940ccb9eb90a6df4c6f2398
Alfie <alfiecollins44@gmail.com>
2025-02-20 23:33:00 +0000

    box model

--- Stylesheet/App.js
+++ Stylesheet/App.js
@@ -21,7 +21,12 @@ const styles = StyleSheet.create({
   box:{
     width:100,
     height:100,
-    padding:10
+    //padding:10
+    paddingHorizontal:10,
+    paddingVertical:20,
+    marginVertical:10,
+    borderWidth: 2,
+    borderColor: 'purple'
   },
   lightBlueBg:{ backgroundColor: "lightblue"},
   lightGreenBg:{ 
