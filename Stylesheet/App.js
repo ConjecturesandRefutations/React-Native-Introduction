d685383da0966f9288ffeaa02a46bddef5491bd7
Alfie <alfiecollins44@gmail.com>
2025-03-20 12:45:39 +0000

    shadow

--- Stylesheet/App.js
+++ Stylesheet/App.js
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
 export default function App(){
   return(
     <View style={styles.container}>
-      <View style={[styles.box, styles.lightBlueBg]}>
+      <View style={[styles.box, styles.lightBlueBg, styles.boxShadow]}>
         <Text>Light Blue Box</Text>
       </View>
       <View style={[styles.box, styles.lightGreenBg]}>
@@ -17,23 +17,37 @@ export default function App(){
 }
 
 const styles = StyleSheet.create({
-  container:{ flex:1, backgroundColor: "plum", padding: 60 },
-  box:{
-    width:100,
-    height:100,
-    //padding:10
-    paddingHorizontal:10,
-    paddingVertical:20,
-    marginVertical:10,
-    borderWidth: 2,
-    borderColor: 'purple'
+  container: {
+    flex: 1,
+    backgroundColor: "#DDA0DD", // plum
+    padding: 60
   },
-  lightBlueBg:{ backgroundColor: "lightblue"},
-  lightGreenBg:{ 
-    backgroundColor: "lightgreen"
-},
-  lightRedBg:{ 
-    backgroundColor: "salmon"
-    }
-}
-)
+  box: {
+    width: 250,
+    height: 250,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    marginVertical: 10,
+    borderWidth: 2,
+    borderColor: '#800080' // purple
+  },
+  lightBlueBg: {
+    backgroundColor: "#ADD8E6" // lightblue
+  },
+  lightGreenBg: {
+    backgroundColor: "#90EE90" // lightgreen
+  },
+  lightRedBg: {
+    backgroundColor: "#FA8072" // salmon
+  },
+  boxShadow: {
+    shadowColor: '#333333',
+    shadowOffset: {
+      width: 6,
+      height: 6
+    },
+    shadowOpacity: 0.6,
+    shadowRadius: 4,
+    elevation: 5 // for Android
+  }
+});
