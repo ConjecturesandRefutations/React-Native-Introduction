c43e88401fefdbf4763c79fef1b3dc511ab9a3fa
Alfie <alfiecollins44@gmail.com>
2025-05-06 12:51:27 +0100

    stuff

--- RN Layout/Components/Box.js
+++ RN Layout/Components/Box.js
@@ -2,8 +2,20 @@ import { View, Text, StyleSheet } from "react-native"
 
 export default function Box(){
     return (
-        <View>
-            <Text>Box</Text>
+        <View style={styles.box}>
+            <Text style={styles.text}>Box</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    box: {
+      backgroundColor: "#fff",
+      padding: 20,
+    },
+    text: {
+      fontSize: 24,
+      fontWeight: "bold",
+      textAlign: "centre"
+    },
+});
